a41d4f0fddb3e50a94b007aacc134ecd5d4b7552
Haimanot Tekie <haimanot.tekie@nav.no>
2025-09-06 15:24:24 +0200

    Form submission

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import solidJs from '@astrojs/solid-js';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astro-frontend-masters.netlify.app',
-  output: 'hybrid',
+  output: 'static',
   integrations: [react(), solidJs()]
 });
