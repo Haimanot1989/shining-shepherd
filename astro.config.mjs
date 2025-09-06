b5154a91c3171d99ecdfcad6c4f66fbe7ca6884e
Haimanot Tekie <haimanot.tekie@nav.no>
2025-09-06 15:36:35 +0200

    Deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,12 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import solidJs from '@astrojs/solid-js';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astro-frontend-masters.netlify.app',
   output: 'static',
-  integrations: [react(), solidJs()]
+  integrations: [react(), solidJs()],
+  adapter: netlify()
 });
